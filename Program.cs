fbeaa236df614a375e3f65874791a823dcf9c54c
eyadsalman547 <121403477+eyadsalman547@users.noreply.github.com>
2022-12-26 14:35:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PerfetNumbers
+{
+    public class PerfectNumbers
+    {
+        public static void Main()
+        {
+            int k, l, s ,first, socend;
+
+            Console.Write("Start Number : ");
+            first = int.Parse(Console.ReadLine());
+            Console.Write("End Number : ");
+            socend = int.Parse(Console.ReadLine());
+
+            for (k = first; k <= socend; k++)
+            {
+                l = 1;
+                s = 0;
+                while (l < k)
+                {
+                    if (k % l == 0)
+                        s = s + l;
+                    l++;
+                }
+                if (s == k)
+                    Console.WriteLine(k + " is perfect number");
+            }
+        }
+    }
+}
